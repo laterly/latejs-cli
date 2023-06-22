a5d544823dffc25579ce86d3a79ec41687afe724
laterly <2608086516@qq.com>
2023-06-22 20:10:34 +0800

    feat: version

--- bin/cli.js
+++ bin/cli.js
@@ -1,37 +1,25 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 
 const program = require("commander");
 const chalk = require("chalk");
-const figlet = require("figlet");
+// const figlet = require("figlet");
+const pkg = require("../package.json");
+
 program
   .command("create <app-name>")
   .description("create a new project")
-  // -f or --force 为强制创建，如果创建的目录存在则直接覆盖
-  .option("-f, --force", "overwrite target directory if it exist")
+  .option("-f, --force", "overwrite target directory if it exists")
   .action((name, options) => {
-    // 打印执行结果
     require("../lib/create.js")(name, options);
   });
+
 program
-  // 配置版本号信息
-  .version(`v${require("../package.json").version}`)
-  .usage("<command> [option]");
+  .version(pkg.version)
+  .option("-v, --version", "output the version number")
+  .on("--help", () => {
+    console.log(
+      `\r\nRun ${chalk.cyan(`late <command> --help`)} for details.\r\n`
+    );
+  });
 
-program.on("--help", () => {
-  // 使用 figlet 绘制 Logo
-  console.log(
-    "\r\n" +
-      figlet.textSync("latejs-cli", {
-        font: "Ghost",
-        horizontalLayout: "default",
-        verticalLayout: "default",
-        width: 80,
-        whitespaceBreak: true,
-      })
-  );
-  // 新增说明信息
-  console.log(`\r\nRun ${chalk.cyan(`peak <command> --help`)} show details\r\n`);
-});
-
-// 解析用户执行命令传入参数
 program.parse(process.argv);
