b5bcd3f0ce7a569fa5886043f3410cbb997721d4
laterly <2608086516@qq.com>
2023-07-01 12:29:27 +0800

    feat: 删除.Git

--- packages/late-cli/lib/generator.js
+++ packages/late-cli/lib/generator.js
@@ -1,8 +1,10 @@
-const path=require('path');
-const chalk=require('chalk');
+const path = require("path");
+const fs = require("fs-extra");
+const chalk = require("chalk");
 const inquirer = require("inquirer");
-const util = require('util')
-const downloadGitRepo = require('download-git-repo') // 不支持 Promise
+const util = require("util");
+const { exec } = require("child_process");
+const downloadGitRepo = require("download-git-repo"); // 不支持 Promise
 const { wrapLoading } = require("./utils");
 const { getRepoList } = require("./http");
 class Generator {
@@ -14,23 +16,24 @@ class Generator {
     // 对 download-git-repo 进行 promise 化改造
     this.downloadGitRepo = util.promisify(downloadGitRepo);
   }
-   // 下载远程模板
+  // 下载远程模板
   // 1）拼接下载地址
   // 2）调用下载方法
   // https://github.com/laterly/react-ts-webpack5.git
-  async download(repo){
+  async download(repo) {
     // 1）拼接下载地址
     const requestUrl = `direct:https://github.com/laterly/${repo}.git`;
 
     // 2）调用下载方法
     await wrapLoading(
       this.downloadGitRepo, // 远程下载方法
-      'waiting download template', // 加载提示信息
+      "waiting download template", // 加载提示信息
       requestUrl, // 参数1: 下载地址
-      path.resolve(process.cwd(), this.targetDir),// 参数2: 创建位置
+      path.resolve(process.cwd(), this.targetDir), // 参数2: 创建位置
       {
-        clone:true
-      }) 
+        clone: true,
+      }
+    );
   }
   // 获取用户选择的模板
   // 1）从远程拉取模板数据
@@ -56,7 +59,7 @@ class Generator {
     // 3）return 用户选择的名称
     return repo;
   }
- 
+
   // 核心创建逻辑
   // 1）获取模板名称
   // 2）获取 tag 名称
@@ -66,13 +69,41 @@ class Generator {
     const repo = await this.getRepo();
     // console.log("用户选择了，repo=" + repo + "，tag=" + tag);
     // 3）下载模板到模板目录
-    await this.download(repo)
-    
-    // 4）模板使用提示
-    console.log(`\r\nSuccessfully created project ${chalk.cyan(this.name)}`)
-    console.log(`\r\n  cd ${chalk.cyan(this.name)}`);
-    console.log(`\r\n  npm install`);
-    console.log('  npm run dev\r\n')
+    await this.download(repo);
+
+    // .git 目录路径
+    // const gitFolderPath = path.resolve(process.cwd(), this.targetDir,'.git');
+    // 删除 .git 目录
+    const gitFolderPath = path.resolve(process.cwd(), this.targetDir, ".git");
+    fs.remove(gitFolderPath, (error) => {
+      if (error) {
+        console.error(`删除 .git 目录时出错：${error}`);
+        return;
+      }
+      //执行 git init 命令
+      exec(
+        "git init",
+        { cwd: path.resolve(process.cwd(), this.targetDir) },
+        (error, stdout, stderr) => {
+          if (error) {
+            console.error(`执行Git初始化时出错：${error}`);
+            return;
+          }
+
+          console.log("Git初始化成功！");
+          // 4）模板使用提示
+          console.log(
+            `\r\nSuccessfully created project ${chalk.cyan(this.name)}`
+          );
+          console.log(`  cd ${chalk.cyan(this.name)}`);
+          console.log(`  pnpm install`);
+          console.log(`  pnpm dev`);
+          console.log(`  pnpm build`);
+          console.log(`  pnpm lint`);
+          console.log("  pnpm lint:style\r\n");
+        }
+      );
+    });
   }
 }
 
